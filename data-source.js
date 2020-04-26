7552a2616ee78ca555ce54e5e271d618568ea7fd
Yemima Inadia <ratnasari.yemima@gmail.com>
2020-04-26 23:20:06 +0700

    setup schedule

--- data-source.js
+++ data-source.js
@@ -71,6 +71,87 @@ $(document).ready(function() {
 
             });
         });
+
+        // tampilkan data resep berdasarkan tanggal yang dipilih
+        $('.dropdown-item').on('click', function() {
+            var scheduleDate = $(this).attr('data-value');
+            //diubah dulu si tanggal yang diklik dari string ke JSON pake JSON Stringify
+            var jsonDate = JSON.stringify({ "date": scheduleDate });
+            var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://cookey-8f6f.restdb.io/rest/recipe?q=" + jsonDate,
+                    "method": "GET",
+                    "headers": {
+                        "content-type": "application/json",
+                        "x-apikey": keyApi,
+                        "cache-control": "no-cache"
+                    }
+                }
+                // minta tampilin hasilnya kalo datanya ada
+            $.ajax(settings).done(function(response) {
+                console.log(response);
+                // hapus resep di html dl baru dioverride sm resep yg baru
+                $('.table-recipe').html('');
+                // trus override 
+                //memisahkan data dari array (copas line#19 persis)
+                $.each(response, function(key, value) {
+
+                    var component = '<div class="col-md-4">' + '<div class="card mb-4 shadow-sm">' +
+                        '<img src="' + value['recipe_poster_url'] + '" alt="" class="bd-placeholder-img card-img-top">' +
+                        '<div class="card-body">' +
+                        '<p class="card-text">' + value['recipe_title'] + '</p>' +
+                        '<div class="d-flex justify-content-between align-items-center">' +
+                        '<div class="btn-group">' +
+                        '<button type="button" class="btn btn-sm btn-outline-secondary btn-view" data-recipeID="' + value['_id'] + '">View</button>' +
+                        '</div>' +
+                        '<small class="text-muted">' + value['recipe_time'] + '</small>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>' +
+                        '</div>';
+                    $('.table-recipe').append(component);
+
+                });
+
+                //showing recipe details lagi berdasarkan id resep yang dipilih
+                // copas lagi dari line #39
+                $('.btn-view').on('click', function() {
+                    //pake this biar ga ke button view yg lain tp ke button view yg diplih
+                    var recipeID = $(this).attr('data-recipeID');
+                    var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": "https://cookey-8f6f.restdb.io/rest/recipe/" + recipeID, //tambah recipeID biar langsung ngarah ke ID yg diminta
+                        "method": "GET",
+                        "headers": {
+                            "content-type": "application/json",
+                            "x-apikey": keyApi,
+                            "cache-control": "no-cache"
+                        }
+                    }
+                    $.ajax(settings).done(function(response) {
+                        console.log(response);
+                        var component = '<div class="col-md-12">' +
+                            '<h1 class="recipe-title">' + response['recipe_title'] + '</h1>' +
+                            '<img src="' + response['recipe_poster_url'] + '" alt="" />' +
+                            '</div>' +
+                            '<div class="col-md-12">' +
+                            '<div class="row">' +
+                            '<div class="col-md-6 ingr"><h3>Ingredients</h3>' + response['recipe_ingredients'] +
+                            '</div>' +
+                            '<div class="col-md-6 steps"><h3>Steps</h3>' + response['recipe_steps'] +
+                            '</div>' +
+                            '</div>' +
+                            '</div>';
+                        $('.detail-recipe .row').append(component);
+                        $('.dashboard-cooking').css('display', 'none');
+                        $('.detail-recipe').css('display', 'block');
+
+                    });
+                });
+            })
+        });
     });
 
 
@@ -110,7 +191,7 @@ $(document).ready(function() {
         return false;
     });
 
-
+    // pindah ke halaman signup/registrasi customer
     $('a.signup').on('click', function() {
         $('.form-signin').css('display', 'none');
         $('.form-signup').css('display', 'block');
